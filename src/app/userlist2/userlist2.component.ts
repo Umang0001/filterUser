3a585d8492e195f2f5662be098c1ace55d448a06
Umang0001 <umangbheda001@gmail.com>
2022-08-22 16:21:11 +0530

    form validation and toggle component

--- src/app/userlist2/userlist2.component.ts
+++ src/app/userlist2/userlist2.component.ts
@@ -16,26 +16,49 @@ export class Userlist2Component implements OnInit {
   ngOnInit(): void {
   }
 
+
+
+  
+  
   handleLogin(data:any){
     console.log(data);
     alert("Login Successful")
     
   }
-
+  
   myForm=new FormGroup({username:new FormControl(""),password: new FormControl("")})
-
+  
+  isName=false;
+  isPass=false
+  passMsg="Enter Password"
+  count=0
+  
   handleSubmit(){
+    this.count++;
+    this.passMsg="Enter Password"
+    this.isName=false
+    this.isPass=false
     if (this.myForm.value.username && this.myForm.value.password) {
+      if (this.myForm.value.password.length<7 &&this.myForm.value.password.length>0) {
+        this.isPass=true;
+        this.passMsg="Minimum 6 characters required"
+      }
       
       console.log(this.myForm.value);
     }
 
     else{
-      alert("Please enter all credentials")
-      
+      if (!this.myForm.value.username) {
+        this.isName=true
+      }
+      if (!this.myForm.value.password) {
+        this.isPass=true
+      }
     }
     
   }
+
+
 
 
 }
